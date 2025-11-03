1c911d942af6cd6f5172f6d844a3ea2de9de19ee
Hugo-Aquino <hugoaquino151@gmail.com>
2025-11-02 22:28:34 -0600

    BaseDatos

--- SI/App.xaml.cs
+++ SI/App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace SI
+﻿using SI.Controllers;
+
+namespace SI
 {
     public partial class App : Application
     {
+        static Controllers.EmpleadosControllers database;
+
+        public static Controllers.EmpleadosControllers Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Controllers.EmpleadosControllers();
+                }
+
+                return database;
+            }
+
+        }
         public App()
         {
             InitializeComponent();
